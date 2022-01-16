509b8bc596554eb30726611ee067691e391f22b6
AlxandrV <a.vigneaud@gmail.com>
2022-01-16 14:22:25 +0100

    Update - add ajax function

--- ocmovies-front/script.js
+++ ocmovies-front/script.js
@@ -1,1 +1,22 @@
-console.log("coucou")
+const API_PATH = "http://localhost:8000/api/v1/titles/"
+
+// Ajax
+function xhr(option){
+
+    return new Promise(function(resolve) {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open(option.type, option.url);
+        xhr.send(option.data);
+    
+        xhr.onreadystatechange = function(){
+                
+            if (this.readyState == 4 && this.status == 200) {
+                let response = xhr.response;
+                return resolve(response);
+            }
+    
+        };
+    });
+}
