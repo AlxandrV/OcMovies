ebe155c0862551a5cbf28de64337a770da25e911
AlxandrV <a.vigneaud@gmail.com>
2022-01-17 12:34:02 +0100

    Update - update to resolve a promise

--- ocmovies-front/script.js
+++ ocmovies-front/script.js
@@ -1,7 +1,7 @@
-const API_PATH = "http://localhost:8000/api/v1/titles/"
+const API_PATH = "http://localhost:8000/api/v1/titles/?format=json"
 
 // Ajax
-function xhr(option){
+function xhr(option) {
     return new Promise(function(resolve) {
         let xhr = new XMLHttpRequest();
         xhr.open(option.type, option.url);
@@ -14,3 +14,9 @@ function xhr(option){
         };
     });
 }
+
+const getJSON = async (option) => {
+    let xhrJSON = await xhr(option).then(JSON.parse)
+    console.log(xhrJSON.results)
+}
+getJSON(option)
